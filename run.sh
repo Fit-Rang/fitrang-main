7f878ab68af939f58afcc56212d5a88dcb8fba15
Noaman Ahmed <noamanahmedai@gmail.com>
2025-08-08 00:42:58 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,6 +5,8 @@ set -e
 echo "📦 Cloning backend services..."
 git clone https://github.com/Fit-Rang/email-verifier.git
 git clone https://github.com/Fit-Rang/profile-service.git
+git clone https://github.com/Fit-Rang/kong.git
+git clone https://github.com/Fit-Rang/keycloak.git
 git clone https://github.com/Fit-Rang/notification_service.git
 git clone https://github.com/Fit-Rang/analysis_service.git
 git clone https://github.com/Fit-Rang/indexing-worker.git
